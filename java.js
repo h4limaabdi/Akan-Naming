0551f7363b8f69aaa7c713d2816a4e755f96da3b
h4lima <h4limaabdi@gmail.com>
2025-09-30 07:49:03 +0300

    Add array for the names

--- java.js
+++ java.js
@@ -2,3 +2,7 @@ const form = document.getElementById('Form');
 const birthDate = document.getElementById('birthDate');
 const gender = getElementById('gender');
 const output = getElementById('output');
+
+//Akan name arrays
+const maleNames = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku", "Yaw", "Kofi" , "Kwame"];
+const femaleNames = ["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"]

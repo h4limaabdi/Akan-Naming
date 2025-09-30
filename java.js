8283ba876178eb88354bc25fe3c65ea7d73382d4
h4lima <h4limaabdi@gmail.com>
2025-09-30 08:18:28 +0300

    Add event listener

--- java.js
+++ java.js
@@ -7,6 +7,26 @@ const output = getElementById('output');
 const maleNames = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku", "Yaw", "Kofi" , "Kwame"];
 const femaleNames = ["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"];
 
+form.addEventListener('Get Name', function(e) {
+    e.preventDefault(); 
+
 //Input values
 const dateValue = birthDate.ariaValueMax;
 const genderValue = gender.value;
+
+//convert string date to object and get day of week
+const objDate = new Date(dateValue);
+const dayOfWeek = objDate.getDay();
+
+//Get name
+let akanName = "";
+if (genderValue === "Male"){
+    akanName = maleNames[dayOfWeek];
+} else
+    if (genderValue === "Female") {
+        akanName = femaleNames[dayOfWeek]
+    }
+
+    //Result 
+    output.textContent = 'Your Akan name is: ${akanName}';
+});
